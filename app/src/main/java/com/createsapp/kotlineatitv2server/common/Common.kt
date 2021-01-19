0ad423a5086730ba01015c8e4d21eb7b72cd17df
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-19 14:48:17 +0530

    Kotlin Android Tutorial - New Eat It part 31 Server App Authentication

---
+++ app/src/main/java/com/createsapp/kotlineatitv2server/common/Common.kt
@@ -0,0 +1,9 @@
+package com.createsapp.kotlineatitv2server.common
+
+import com.createsapp.kotlineatitv2server.model.ServerUserModel
+
+object Common {
+
+    val SERVER_REF = "Server"
+    var currentServerUser: ServerUserModel?=null
+}
---
+++ app/src/main/java/com/createsapp/kotlineatitv2server/model/ServerUserModel.kt
@@ -0,0 +1,8 @@
+package com.createsapp.kotlineatitv2server.model
+
+class ServerUserModel {
+    var uid: String?=null
+    var name:String?=null
+    var phone:String?=null
+    var isActive:Boolean = false
+}
